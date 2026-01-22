7748e39c2d0663ab02a2ad52463de9f0f1a9f5fd
Eduardo Schefer <eduardoschefer@192.168.1.53>
2026-01-22 09:05:02 -0300

    Config: Ignore TS/Lint errors on build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
